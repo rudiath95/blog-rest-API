0b22137bec3483963e8ba735778608c59c0fa62d
Rudiath Rizky Adi Nugraha <rudiath.r@gmail.com>
2023-03-07 19:15:29 +0700

    done for today

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rudiath95/blog-rest-API/ini"
+)
 
 func init() {
-
+	ini.LoadEnvVariables()
+	ini.ConnecttoDB()
+	ini.SyncDatabases()
 }
 
 func main() {
-	fmt.Println("COCK")
+	r := gin.Default()
+	//>>Start Route
+
+	//>>End Route
+	r.Run()
 }
--- models/blog.go
+++ models/blog.go
@@ -6,8 +6,8 @@ import (
 
 type Blog struct {
 	gorm.Model
-	User_ID        int
-	OriginalPoster User `gorm:"foreignKey:User_ID"`
+	OriginalPoster string
+	Username       User `gorm:"foreignKey:OriginalPoster"`
 	Content        string
 }
 
@@ -15,7 +15,7 @@ type Comment struct {
 	gorm.Model
 	Blog_ID        int
 	Blog           Blog `gorm:"foreignKey:Blog_ID"`
-	User_ID        int
-	User           User `gorm:"foreignKey:Blog_ID"`
+	Commenter      string
+	Username       User `gorm:"foreignKey:Commenter"`
 	CommentContent string
 }
